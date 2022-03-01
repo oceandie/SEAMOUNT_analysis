74ed0d5f4f16b277cb0e8574b4a97115d26509ee
oceandie <diego.bruciaferri@metoffice.gov.uk>
2022-03-01 12:04:39 +0000

    code to plot correctly model sections and umax timeseries

--- src/python/plot_seamount_rmax.py
+++ src/python/plot_seamount_rmax.py
@@ -2,15 +2,15 @@
 
 #     |------------------------------------------------------------|
 #     | Author: Diego Bruciaferri                                  |
-#     | Date and place: 07-09-2021, Met Office, UK                 |
+#     | Date and place: 28-02-2022, Met Office, UK                 |
 #     |------------------------------------------------------------|
 
 
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.patches as patches
+import matplotlib.cm as cm
 import xarray as xr
-import cmocean
 
 def calc_rmax(depth: xr.DataArray) -> xr.DataArray:
     """
@@ -74,8 +74,10 @@ def calc_rmax(depth: xr.DataArray) -> xr.DataArray:
 # 1. INPUT FILES
 
 meshmask = ['/data/users/dbruciaf/HPG/SEAMOUNT/N-DJC-CTR_steep/mesh_mask.nc',
-            '/data/users/dbruciaf/HPG/SEAMOUNT/N-DJC-CTR_moderate/mesh_mask.nc']
-conf = ['s','m']
+            '/data/users/dbruciaf/HPG/SEAMOUNT/N-DJC-CTR_moderate/mesh_mask.nc',
+            '/data/users/dbruciaf/HPG/SEAMOUNT/N-DJC-SH94_moderate/mesh_mask.nc',
+            '/data/users/dbruciaf/HPG/SEAMOUNT/N-DJC-VQS_moderate/mesh_mask.nc']
+conf = ['ctr-s','ctr-m','sh94-m','vqs-m']
 
 fig_path = '/home/h01/dbruciaf/mod_dev/SEAMOUNT_analysis/plots' 
 
@@ -87,50 +89,120 @@ for exp in range(len(meshmask)):
     ds_dom = xr.open_dataset(meshmask[exp], drop_variables=("x", "y"))
 
     # Computing rmax
-    rmax  = calc_rmax(ds_dom.bathymetry.squeeze())
+    rmax  = calc_rmax(ds_dom.gdepw_0.squeeze().isel(nav_lev=-1))
+    rmax  = rmax.drop_vars("nav_lev")
+ 
+    # Adding 3rd dimension for plotting
     rmax  = rmax.expand_dims({"nav_lev": 20})
     glamt = ds_dom.glamt.expand_dims({"nav_lev": 20})
     glamu = ds_dom.glamu.expand_dims({"nav_lev": 20})
 
     # Plotting ----------------------------------------------------------
 
-    levt_y = ds_dom.gdept_0.isel(y=32).squeeze()
-    levw_y = ds_dom.gdepw_0.isel(y=32).squeeze()
-    glamt_y = glamt.isel(y=32).squeeze()
-    glamu_y = glamu.isel(y=32).squeeze()
+    bathy_m = ds_dom.bathymetry.isel(y=31).squeeze()
 
-    rmax_y = rmax.isel(y=32).squeeze()
+    levT_y = ds_dom.gdept_0.isel(y=31).squeeze()
+    levW_y = ds_dom.gdepw_0.isel(y=31).squeeze()
+    glamT_y = glamt.isel(y=31).squeeze()
+    tmask_y = ds_dom.tmask.isel(y=31).squeeze()
+    tmask_y = tmask_y.drop_vars("nav_lev")
+
+    rmax_y = rmax.isel(y=31).squeeze()
+    rmax_y = rmax_y.where(tmask_y==1)
 
     fig = plt.figure(figsize=(30, 20), dpi=100)
     ax = fig.add_subplot()
     ax.invert_yaxis()
+    ax.set_facecolor('silver')
 
-    # rmax
-    pc = ax.pcolormesh(glamt_y, levw_y, rmax_y, cmap='hot_r')
+    # RMAX -----------------------------------------------
+    
+    # Creating grid for plotting T-grid values
+    # at the centre of the cells
+    ni = len(levT_y.x)
+    nk = len(levT_y.nav_lev)
 
-    # W-levels and T-points
+    # Interpolating T-depths on U and W grid
+    levUT_y = levT_y.rolling({"x": 2}).mean().fillna(levT_y[:, 0])
+    levUW_y = levUT_y.rolling({"nav_lev": 2}).mean().fillna(0.0) # surface
+
+    # Interpolating T-coord. on U grid
+    glamU_y = glamT_y.rolling({"x": 2}).mean()
+    glamU_y[:, 0] = glamT_y[:, 0] - (glamU_y[:, 1] - glamT_y[:, 0])
+
+    # Creating arrays with shape (nk+1,n+1) 
+    levUW_yp1 = np.zeros(shape=(nk+1, ni+1))
+    glamU_yp1 = np.zeros(shape=(nk+1, ni+1))
+
+    levUW_yp1[:-1, :-1] = levUW_y.data
+    glamU_yp1[:-1, :-1] = glamU_y.data
+
+    cmap = cm.get_cmap("hot_r").copy()
+    #cmap.set_under(color='silver')
+ 
+    if '-m' in conf[exp]:
+       pc = ax.pcolormesh(
+                  glamU_yp1, 
+                  levUW_yp1, 
+                  rmax_y, 
+                  cmap=cmap,
+                  vmin=0.,
+                  vmax=0.07 
+            )
+    else:
+       pc = ax.pcolormesh(
+                  glamU_yp1, 
+                  levUW_yp1,
+                  rmax_y,
+                  cmap=cmap
+            )
+
+    # MODEL W-levels and T-points ----------------------------
     for k in range(len(ds_dom.nav_lev)):
         ax.plot(
-            glamt_y.isel(nav_lev=k),
-            levw_y.isel(nav_lev=k),
+            glamT_y.isel(nav_lev=k),
+            levW_y.isel(nav_lev=k),
             color="k",
+            zorder=5
         )
         ax.scatter(
-            glamt_y.isel(nav_lev=k),
-            levt_y.isel(nav_lev=k),
+            glamT_y.isel(nav_lev=k),
+            levT_y.isel(nav_lev=k),
             s = 5.0,
             color="k",
+            #zorder=5
         )
 
-    # Mesh-lines
     for i in range(len(ds_dom.x)):
         ax.plot(
-           glamu_y.isel(x=i),
-           levw_y.isel(x=i),
+           [glamU_y.isel(x=i)[0],
+            glamU_y.isel(x=i)[-1]],
+           [np.nanmin(levW_y),
+            np.nanmax(levW_y)],
            color="k",
            linestyle='--'
         )
+
+    # MODEL BATHYMETRY
+    #path = [
+    #    [glamT_y[-1, 0], levT_y[-1, 0]],
+    #    [glamT_y[-1, -1], levT_y[-1, -1]],
+    #    ]
+    #for i in range(len(levW_y.x)):
+    #    path.append([glamT_y[-1, i], levW_y[-1, i]])
+    #ax.add_patch(patches.Polygon(path, facecolor="silver", zorder=4))
+
+    #path = [
+    #    [glamU_y[-1,  0], bathy_m[ 0]],
+    #    [glamU_y[-1, -1], bathy_m[-1]],
+    #    ]
+    #for i in range(len(bathy_m.x)):
+    #    path.append([glamT_y[-1, i], levW_y[-1, i]])
+    #ax.add_patch(patches.Polygon(path, facecolor="silver", zorder=4, alpha=0.4))
+
+    # PLOT setting ----------------------------
     ax.set_ylim(4500.0, 0.0)
+    ax.set_xlim(8, 500)
     cb = plt.colorbar(pc)
     cb.set_label("Slope parameter", size=40)
     cb.ax.tick_params(labelsize=30)
@@ -139,13 +211,6 @@ for exp in range(len(meshmask)):
     plt.yticks(fontsize=30.)
     plt.ylabel('Depth [m]' ,fontsize=40.)
 
-    path = [
-        [glamt_y[-1, 0], levt_y[-1, 0]],
-        [glamt_y[-1, -1], levt_y[-1, -1]],
-        ]
-    for i in range(len(levw_y.x)):
-        path.append([glamt_y[-1, i], levw_y[-1, i]])
-    ax.add_patch(patches.Polygon(path, facecolor="silver", zorder=4))
 
     fig_name = '/seamount_'+conf[exp]+'_rmax.png'
     plt.savefig(fig_path+fig_name, bbox_inches="tight")
